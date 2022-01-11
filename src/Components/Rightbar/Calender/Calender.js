e510673f3f5b83c7d423fb4be8d94c22373af531
utsav-12 <utsavjain6913@gmail.com>
2022-01-11 17:06:30 +0530

    updated

--- src/Components/Rightbar/Calender/Calender.js
+++ src/Components/Rightbar/Calender/Calender.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import "./Calender.css"
 import Cal from './Cal/Cal'
 
 const Calender = () => {
     return (
         <div className='calender'>
-            
+
             <p className='text_rightbar'>upcomming classes </p>
-            <Cal/>
+            <Cal />
         </div>
     )
 }
